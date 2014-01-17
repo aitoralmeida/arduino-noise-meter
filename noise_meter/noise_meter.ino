54d62511822719be6389c7bd0827c55875c6b3f3
Aitor Almeida <aitoralmeida@gmail.com>
2014-01-17 09:06:01 +0100

    first working example communicating with the philips hue lightbulb

--- noise_meter/noise_meter.ino
+++ noise_meter/noise_meter.ino
@@ -7,13 +7,12 @@ int noise = 0;
 //Mac address of the ethernet shield
 byte mac[] = {  0x90, 0xA2, 0xDA, 0x00, 0x79, 0xC1 };
 //Philips Hue server
-IPAddress server(10,164,37,209);
-int port = 4444;
+//IPAddress server(10,164,37,209);
+IPAddress server(10,48,1,158);
+//int port = 4444;
+int port = 80;
 
 EthernetClient client;
-
-
-
 
 
 void setup(){
@@ -27,14 +26,14 @@ void setup(){
   }
   delay(1000);
   Serial.println("connecting...");
-
-  do_get("/philipshue?foo=bar");  
+  
+  do_put("newdeveloper", "1", "{\"hue\": 11730,\"on\": true, \"bri\": 10, \"sat\": 255}");  
 }
 
 void loop()
 {
   noise = getNoiseLevel();    
-  Serial.println(noise);   
+  //Serial.println(noise);   
 
   if (!client.connected()) {
     Serial.println();
@@ -71,7 +70,7 @@ void do_get(String msg){
   }
 }
 
-void do_post(String msg){
+void do_post(String username, String id, String msg){
   Serial.println("doing post");
   
   String postData = msg;
@@ -79,9 +78,44 @@ void do_post(String msg){
 
   if (client.connect(server, port)) {
     Serial.println("connected");
-    client.println("POST /HueControl HTTP/1.1");
+    client.print("POST ");
+    client.print("/api/");
+    client.print(username);
+    client.print("/lights/");
+    client.print(id);
+    client.print("/state");
+    client.println(" HTTP/1.1");
     client.println("Host: 10.164.37.209");
-    client.println("User-Agent: Arduino/1.0");
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.println("Connection: close");
+    client.print("Content-Length: ");
+    client.println(postData.length());
+    client.println();
+    client.println(postData);
+  }
+  else {
+    Serial.println("connection failed");
+  }
+}
+
+
+void do_put(String username, String id, String msg){
+  Serial.println("doing put");
+  
+  String postData = msg;
+
+
+  if (client.connect(server, port)) {
+    Serial.println("connected");
+    client.print("PUT ");
+    client.print("/api/");
+    client.print(username);
+    client.print("/lights/");
+    client.print(id);
+    client.print("/state");
+    client.println(" HTTP/1.1");
+    client.println("Host: 10.164.37.209");
+    client.println("Content-Type: application/json");
     client.println("Connection: close");
     client.print("Content-Length: ");
     client.println(postData.length());
